b2d23875f6c30868cf2b8b5f8f873c50fb4ec42e
Patrick Gallagher <blockchainbuddha@gmail.com>
2020-05-19 19:38:59 -0400

    add LocalizationProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,15 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { LocalizationProvider } from "gatsby-plugin-internationalization"
 
 import Layout from "./src/components/layout"
 
-export const wrapPageElement = ({ element }) => {
-  return <Layout>{element}</Layout>
+export const wrapPageElement = ({ element, props }) => {
+  return (
+    <LocalizationProvider {...props}>
+      <Layout>{element}</Layout>
+    </LocalizationProvider>
+  )
 }
 
 wrapPageElement.propTypes = {
